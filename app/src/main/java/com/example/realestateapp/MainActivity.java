3f08e53acba9a1c7a8dec980c347d08044c78777
edwin <edwinblaauw141@gmail.com>
2022-11-05 17:15:43 +0200

    commit

---
+++ app/src/main/java/com/example/realestateapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.realestateapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.realestateapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void ButtonClick(View view)
+    {
+        EditText et1= (EditText) findViewById(R.id.Myemail);
+        EditText et2= (EditText) findViewById(R.id.Mypass);
+
+        String email= et1.getText().toString();
+        String pass = et2.getText().toString();
+        
+        String oemail= "admin";
+        String opass = "admin";
+
+        if (email.equals(oemail) && pass.equals(opass))
+        {
+            Intent intent = new Intent(this, Navigation.class);
+            startActivity(intent);
+
+
+
+        }
+        else
+        {
+            Toast.makeText(this, "Invalid Email/Password", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
